c17b98583290b5736556afd0b7e74bbce8e65974
Jihad Ballout <148473573+yeezySznApproaching@users.noreply.github.com>
2025-01-28 18:34:45 +0100

    uses UIImagePickerController under the hood and returns the Data for the selected image

--- MovieLibrary/Utilities/ImagePicker.swift
+++ MovieLibrary/Utilities/ImagePicker.swift
@@ -1,8 +1,41 @@
-//
-//  ImagePicker.swift
-//  MovieLibrary
-//
-//  Created by Jihad Ballout on 28/01/2025.
-//
+import SwiftUI
+import PhotosUI
 
-import Foundation
+struct ImagePickerView: UIViewControllerRepresentable {
+    @Binding var selectedData: Data?
+
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.sourceType = .photoLibrary
+        picker.delegate = context.coordinator
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+        
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        let parent: ImagePickerView
+
+        init(_ parent: ImagePickerView) {
+            self.parent = parent
+        }
+
+        func imagePickerController(
+            _ picker: UIImagePickerController,
+            didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]
+        ) {
+            picker.dismiss(animated: true)
+
+            if let image = info[.originalImage] as? UIImage,
+               let data = image.jpegData(compressionQuality: 0.8) {
+                parent.selectedData = data
+            }
+        }
+    }
+}
