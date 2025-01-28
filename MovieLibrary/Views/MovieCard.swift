04be76508eda92e45b21ce6a4745348e9c02e530
Jihad Ballout <148473573+yeezySznApproaching@users.noreply.github.com>
2025-01-28 17:54:26 +0100

    displays poster + title/year/rating

--- MovieLibrary/Views/MovieCard.swift
+++ MovieLibrary/Views/MovieCard.swift
@@ -1,18 +1,37 @@
-//
-//  MovieCard.swift
-//  MovieLibrary
-//
-//  Created by Jihad Ballout on 28/01/2025.
-//
-
 import SwiftUI
 
 struct MovieCard: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+    let movie: Movie
 
-#Preview {
-    MovieCard()
+    var body: some View {
+        HStack(alignment: .top) {
+            // Poster image from binary data
+            if let data = movie.posterData,
+               let uiImage = UIImage(data: data) {
+                Image(uiImage: uiImage)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 80, height: 120)
+                    .cornerRadius(8)
+            } else {
+                // Placeholder if no poster
+                Rectangle()
+                    .fill(Color.gray.opacity(0.2))
+                    .frame(width: 80, height: 120)
+                    .cornerRadius(8)
+            }
+
+            // Text info
+            VStack(alignment: .leading, spacing: 6) {
+                Text(movie.title ?? "Untitled")
+                    .font(.headline)
+                Text("Year: \(movie.year)")
+                    .font(.subheadline)
+                Text("Rating: \(movie.rating, specifier: "%.1f")/5")
+                    .font(.subheadline)
+            }
+            Spacer()
+        }
+        .padding(.vertical, 8)
+    }
 }
