55c7887fb8456ba8901d1fe1b615dfc61617ea7d
Jihad Ballout <148473573+yeezySznApproaching@users.noreply.github.com>
2025-01-28 16:58:46 +0100

    created a models folder and a views folder and placed some files in them

---
+++ MovieLibrary/Views/AddMovieView.swift
@@ -0,0 +1,18 @@
+//
+//  AddMovieView.swift
+//  MovieLibrary
+//
+//  Created by Jihad Ballout on 28/01/2025.
+//
+
+import SwiftUI
+
+struct AddMovieView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddMovieView()
+}
